b8893e244c9c7ef684b73e5fc1fd0f771d3f2bf3
becode <yf_be@yahoo.co.uk>
2020-08-14 14:35:38 +0200

    finished required steps

--- src/Controller/LearningController.php
+++ src/Controller/LearningController.php
@@ -3,14 +3,18 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class LearningController extends AbstractController
 {
     /**
      * @Route("/learning", name="learning")
      */
-    public function index(): \Symfony\Component\HttpFoundation\Response
+    public function index(): Response
     {
         return $this->render('learning/index.html.twig', [
             'controller_name' => 'LearningController',
@@ -18,22 +22,52 @@ class LearningController extends AbstractController
     }
 
     /**
-     * @Route("/aboutMe", name="about")
+     * @Route("/aboutBecode", name="aboutMe")
+     * @param SessionInterface $session
+     * @return Response
      */
-    public function aboutMe()
+    public function aboutMe(SessionInterface $session): Response
     {
-        return $this->render('aboutMe.html.twig', [
-            'name' => 'BeCode'
+        if ($session->get('name')) {
+            $name = $session->get('name');
+            $response = $this->render('aboutMe.html.twig', [
+                'name' => $name
+            ]);
+        } else {
+            $response = $this->forward('App\Controller\LearningController::showMyName');
+        }
+        return $response;
+    }
+
+    /**
+     * @Route("/", name="showMyName")
+     * @param SessionInterface $session
+     * @return Response
+     */
+    public function showMyName(SessionInterface $session): Response
+    {
+        if ($session->get('name')) {
+            $name = $session->get('name');
+        } else {
+            $name = 'Unknown';
+
+        }
+        return $this->render('showMyName.html.twig', [
+            'name' => $name
         ]);
     }
 
     /**
-     * @Route("/showMyName", name="show")
+     * @Route("/changeMyName", name="changeMyName", methods={"POST"})
+     * @param Request $request
+     * @param SessionInterface $session
+     * @return RedirectResponse
      */
-    public function showMyName()
+    public function changeMyName(Request $request, SessionInterface $session): RedirectResponse
     {
-        return $this->render('showMyName.html.twig', [
-            'name' => 'Unknown'
-        ]);
+        $name = $request->request->get('name');
+        $session->set('name', $name);
+
+        return $this->redirectToRoute('showMyName');
     }
 }
